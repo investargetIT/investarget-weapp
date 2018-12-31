0173f80b01ad5de9caa53838be3dd761697cac39
InvesTarget <system@investarget.com>
2018-12-31 15:45:10 +0800

    动态获取活动地址

--- app.js
+++ app.js
@@ -28,6 +28,21 @@ App({
         }
       }
     })
+
+    wx.request({
+      url: 'https://api.investarget.com/activity',
+      data: { page_size: 100 },
+      method: 'GET',
+      success: res => {
+        if (res.data.result.count > 0) {
+          const events = res.data.result.data.filter(f => !f.isNews);
+          if (events.length > 0) {
+            this.globalData.eventUrl = events[0].detailUrl;
+          }
+        }
+      }
+    })
+
   },
   url (uri) {
     return this.globalData.baseURL + uri;
@@ -41,6 +56,7 @@ App({
     latestInfo: null,
     userInfo: null,
     devMode: false,
-    baseURL: "https://m.investarget.com"
+    baseURL: "https://m.investarget.com",
+    eventUrl: 'https://mp.weixin.qq.com/s?__biz=MzIyMjIxODkzNQ==&mid=2652458533&idx=1&sn=7354f560a7a19ead4c045abe0d284a6f&chksm=f3dd31fdc4aab8eb8a30ce44555d2babffe12f0ae24eb2118ec1b0823ad1e9ff993b6a0bfd11#rd',
   }
 })
--- pages/event/event.js
+++ pages/event/event.js
@@ -6,5 +6,7 @@ Page({
   data: {
     url: "https://mp.weixin.qq.com/s?__biz=MzIyMjIxODkzNQ==&mid=2652458533&idx=1&sn=7354f560a7a19ead4c045abe0d284a6f&chksm=f3dd31fdc4aab8eb8a30ce44555d2babffe12f0ae24eb2118ec1b0823ad1e9ff993b6a0bfd11#rd"
   },
-  onLoad: function () {}
+  onTabItemTap () {
+    this.setData({ url: app.globalData.eventUrl });
+  }
 })
