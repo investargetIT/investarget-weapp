81263daf70408539452726b88bf0fabad883517a
InvesTarget <system@investarget.com>
2019-01-02 11:47:49 +0800

    修改写死的活动页链接，发布v0.1.4

--- pages/event/event.js
+++ pages/event/event.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    url: "https://mp.weixin.qq.com/s?__biz=MzIyMjIxODkzNQ==&mid=2652458533&idx=1&sn=7354f560a7a19ead4c045abe0d284a6f&chksm=f3dd31fdc4aab8eb8a30ce44555d2babffe12f0ae24eb2118ec1b0823ad1e9ff993b6a0bfd11#rd"
+    url: "https://mp.weixin.qq.com/s/iInCoTAPaEthnfIhoM_6Ew"
   },
   onTabItemTap () {
     this.setData({ url: app.globalData.eventUrl });
